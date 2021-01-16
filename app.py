55c58e5b3bfb2452fc1b8f20e46a90b6147f3cbc
addison-ch <addisonchen.py@gmail.com>
2021-01-15 23:42:33 -0500

    changed airborn to app

---
+++ app.py
@@ -0,0 +1,14 @@
+# testing
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('landingpage.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
