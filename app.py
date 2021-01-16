4bc48ee1cd9a3f059fbea3ffe331509a5f7c7b2c
jutinwang <75459321+jutinwang@users.noreply.github.com>
2021-01-16 09:52:26 -0500

    New version with disease page

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def landingpage():
     return render_template('landingpage.html')
 
+@app.route('/disease')
+def disease():
+    return render_template('disease.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
