0a5ef132f1bc8af5d2e5430fb2d1f0fe536676ad
addison-ch <addisonchen.py@gmail.com>
2021-01-16 16:07:32 -0500

    backend_testing

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # testing
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def landingpage():
     return render_template('landingpage.html')
+
+
+@app.route('/country/<string:name>')
+def country_data(name):
+    return render_template('country.html', country=name)
 
 
 @app.route('/disease')
