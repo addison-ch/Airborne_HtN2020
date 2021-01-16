d072363fc3fe3cc75fc5552b816d3748dfd2a192
jutinwang <75459321+jutinwang@users.noreply.github.com>
2021-01-15 23:33:40 -0500

    Added comments that describe what the program does

--- airborn.py
+++ airborn.py
@@ -1,3 +1,3 @@
-# This is Jerry's test comment.
-# This is Jerry's second test comment.
-# this is a test comment
+# This is the main body of the program
+# This program uses weather and disease data to inform people about diseases.
+# This program will be useful to those who are travelling to other, less developed countries
