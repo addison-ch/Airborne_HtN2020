34cf39e2de0059a0420a8ddc719a2103c5fc4701
jutinwang <75459321+jutinwang@users.noreply.github.com>
2021-01-16 09:34:57 -0500

    Made a new html page and just fiddled around

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 # testing
-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+def landingpage():
     return render_template('landingpage.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
