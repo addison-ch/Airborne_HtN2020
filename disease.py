cc7451a48ae4a476fe75b11a5a7905ce40c3cf77
jerrykpan <jerrykpan@gmail.com>
2021-01-17 01:21:58 -0500

    Update disease.py
    Removed old code

--- disease.py
+++ disease.py
@@ -30,16 +30,10 @@ def get_worse_disease(country: str):
     # getting the endpoint
     wd_url = 'https://query.dropbase.io/kkBkXojaa8xcgbrN4dsDUN/countries?country=eq.' + country
     # making the request
-<<<<<<< Updated upstream
-    x = requests.get(url, headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhYmFzZUlkIjoia2tCa1hvamFhOHhjZ2JyTjRkc0RVTiIsImFjY2Vzc1Blcm0iOiJmdWxsIiwidG9rZW5JZCI6IkhDWEdiWGZoOFBFRk1oMWc5djJzQTlkTUs2cE1qTnJJWUVJNFVDSU00S2tjRndReDd0eGRlVkdOTGhNQldseE8iLCJpYXQiOjE2MTA4NDgzNDIsImV4cCI6MTYxMDk0OTE0MiwiaXNzIjoiZHJvcGJhc2UuaW8iLCJzdWIiOiJoZHVEckxDN3VoWDhRakNybk5CQ0JrIn0.ZeO3qEL60YIQWH6dHSPWleBJoBw2IZLPMxl1-HE0XVU"})
-    response_json = x.json()             # JSON format of query
-    country = response_json[0]           # getting the first entry
-=======
     wd_response = requests.get(wd_url, headers={"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhYmFzZUlkIjoia2tCa1hvamFhOHhjZ2JyTjRkc0RVTiIsImFjY2Vzc1Blcm0iOiJmdWxsIiwidG9rZW5JZCI6IkhDWEdiWGZoOFBFRk1oMWc5djJzQTlkTUs2cE1qTnJJWUVJNFVDSU00S2tjRndReDd0eGRlVkdOTGhNQldseE8iLCJpYXQiOjE2MTA4NDgzNDIsImV4cCI6MTYxMDk0OTE0MiwiaXNzIjoiZHJvcGJhc2UuaW8iLCJzdWIiOiJoZHVEckxDN3VoWDhRakNybk5CQ0JrIn0.ZeO3qEL60YIQWH6dHSPWleBJoBw2IZLPMxl1-HE0XVU"})
     wd_response_json = wd_response.json()                           # JSON format of query
     # print("\n" + json.dumps(wd_response_json, indent=4) + "\n")
     country = wd_response_json[0]                                   # getting the first entry
->>>>>>> Stashed changes
     return country['largest_disease']
 
 
